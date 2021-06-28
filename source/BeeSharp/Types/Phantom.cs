ebc73674bfe6c69c81f82647666d84de16feef5a
Philipp Haiden <paiden@outlook.com>
2021-06-28 18:25:36 +0200

    Add phantom type

---
+++ source/BeeSharp/Types/Phantom.cs
@@ -0,0 +1,15 @@
+﻿namespace BeeSharp.Types
+{
+    public record Phantom<T, TPhantomTrait>
+    {
+        private readonly T value;
+
+        public Phantom(T value)
+        {
+            this.value = value;
+        }
+
+        public static implicit operator T(Phantom<T, TPhantomTrait> phantom)
+            => phantom.value;
+    }
+}
