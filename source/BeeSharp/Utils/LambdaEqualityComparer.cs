e81314a9589add2823fd4748e198f379e055ef35
Philipp Haiden <paiden@outlook.com>
2021-09-22 09:29:12 +0200

    Add Lambda equality comparer

---
+++ source/BeeSharp/Utils/LambdaEqualityComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeeSharp.Utils
+{
+    public sealed class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> equals;
+        private readonly Func<T, int> getHashCode;
+
+        public LambdaEqualityComparer(Func<T, T, bool> equals)
+            : this(equals, x => x.GetHashCode())
+        {
+        }
+
+        public LambdaEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+        {
+            this.equals = equals;
+            this.getHashCode = getHashCode;
+        }
+
+        public bool Equals(T x, T y)
+            => this.equals(x, y);
+
+        public int GetHashCode(T obj)
+            => this.getHashCode(obj);
+    }
+}
